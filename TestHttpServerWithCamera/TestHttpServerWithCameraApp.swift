de1e78f541094255eca317ca4df65215d9541da8
Konrad Pagacz <konrad.pagacz@gmail.com>
2025-06-27 09:47:22 +0200

    Initial Commit

---
+++ TestHttpServerWithCamera/TestHttpServerWithCameraApp.swift
@@ -0,0 +1,32 @@
+//
+//  TestHttpServerWithCameraApp.swift
+//  TestHttpServerWithCamera
+//
+//  Created by Konrad Pagacz on 27/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TestHttpServerWithCameraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
